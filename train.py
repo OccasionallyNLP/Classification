ad30057295cd69f9a1138f6c8b7f6f52753d3a32
OccasionallyNLP <111713221+OccasionallyNLP@users.noreply.github.com>
2023-06-22 10:45:25 +0900

    Update train.py

--- train.py
+++ train.py
@@ -321,9 +321,9 @@ if __name__=='__main__':
     weights = None
     if args.weighted_loss:
         labels = [i['label'] for i in train_dataset]
-        weights = [1/labels.count(c) for c in range(args.n_labels)]
+        weights = [1-labels.count(c)/sum(labels) for c in range(args.n_labels)]
         if args.n_labels == 1:
-            weights.append(1/labels.count(1))
+            weights.append(1-labels.count(1)/sum(labels))
         weights = torch.tensor(weights)
     args.weights = weights.tolist() if weights is not None else weights
         
